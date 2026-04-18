63f3ba0f6e790551776b52b4eb89d1b617c568fd
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-04-18 18:30:12 +0000

    Add files via upload

---
+++ program103.java
@@ -0,0 +1,54 @@
+//Input ; 4   4
+/*
+    * * * *
+    o o o o
+    o o o o
+    * * * *
+ */
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0, iCnt = 1;
+
+        for(i = 1; i <= iRow; i++ )
+        {
+                for(j = 1; j <= iCol; j++, iCnt++)
+                {
+                    if((i == 1) || (i == iRow))
+                    {
+                        System.out.print("*\t");
+                    }
+                    else
+                    {
+                        System.out.print("o\t");
+                    }
+                        
+                }
+            
+            System.out.println();
+        }
+    }
+}
+class program103
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
