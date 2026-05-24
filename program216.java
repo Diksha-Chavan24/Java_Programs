08badab11573bdf8736a96a6a4f2275f35a0acc1
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-24 17:45:34 +0000

    Add files via upload

---
+++ program216.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class program216
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of Elements :");
+        int iSize = sobj.nextInt();
+
+        int Arr[] = new int[iSize];
+
+        int iCnt = 0;
+
+        System.out.println("Enter the Elements : ");
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            Arr[iCnt] = sobj.nextInt();
+        }
+
+        System.out.println("Elements of the array are : ");
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+
+        int iCount = 0;
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] % 2) == 0)
+            {
+                iCount++;
+            }
+        }
+
+        System.out.println("Elements of even numbers : "+iCount);
+
+        Arr = null;
+        sobj = null;
+    }
+}
