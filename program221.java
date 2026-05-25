097a5a378983a60ac3bd495c8e59866e1c88fe00
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-25 18:04:24 +0000

    Add files via upload

---
+++ program221.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+class NNumberX
+{
+    public int Arr[];
+
+    public NNumberX(int iSize)
+    {
+        System.out.println("Allocating the resources...");
+        Arr = new int[iSize];           //Resource Allocation
+    }
+
+    protected void finalize()
+    {
+        System.out.println("Deallocating the resources...");
+        Arr = null;
+    }
+
+    public void Accept()                //getter method
+    {
+        int iCnt = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Elements : ");
+        for(iCnt = 0; iCnt < this.Arr.length; iCnt++)
+        {
+            this.Arr[iCnt] = sobj.nextInt();
+        }
+
+        sobj = null;
+    }
+
+    public void Display()
+    {
+        int iCnt = 0;
+
+        System.out.println("Elements of the array are : ");
+        for(iCnt = 0; iCnt < this.Arr.length; iCnt++)
+        {
+            System.out.println(this.Arr[iCnt]);
+        }
+    }
+
+    public int CountEven()
+    {
+        int iCount = 0, iCnt =0;
+
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] % 2) == 0)
+            {
+                iCount++;
+            }
+        }
+
+        return iCount;
+    }
+}
+
+class program221
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of Elements :");
+        int iSize = sobj.nextInt();
+
+        NNumberX nobj = new NNumberX(iSize);
+
+        nobj.Accept();
+
+        nobj.Display();
+
+        int iRet = 0;
+       
+        iRet = nobj.CountEven();
+
+        System.out.println("Elements of even numbes are : "+iRet);
+        
+        sobj = null;
+
+        System.gc();
+    }
+}
